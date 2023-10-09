830befe94a70e8a5d371b69aba89f93834ac56f3
kanatsumi <naduccva@gmail.com>
2023-10-09 17:08:48 +0700

    hello

--- src/main/java/com/fzoo/zoomanagementsystem/controller/AnimalController.java
+++ src/main/java/com/fzoo/zoomanagementsystem/controller/AnimalController.java
@@ -1,4 +1,14 @@
 package com.fzoo.zoomanagementsystem.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("Animal")
 public class AnimalController {
+@GetMapping
+    public String animal(){
+    return "Hello";
+}
 }
